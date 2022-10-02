fe43f15f2c8c93e5755ee4afea8ec8128622fd4d
12CUY <wiliampuntomorales45@gmail.com>
2022-10-02 01:15:02 -0500

    daatas

--- Proyecto_Kid_Helped_Base_Admin/src/app.js
+++ Proyecto_Kid_Helped_Base_Admin/src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const morgan = require("morgan"); 
+const morgan = require("morgan");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
@@ -9,16 +9,16 @@ const mysqlstore = require("express-mysql-session")(session);
 const bodyParser = require("body-parser");
 const fileupload = require("express-fileupload");
 
-const {database}=require('./keys')
+const { database } = require('./keys')
 
 //Inicializacion
-const app = express();    
+const app = express();
 require('./lib/passport')
 
-const handlebars=exphbs.create({
+const handlebars = exphbs.create({
     defaultLayout: "main",
-    layoutsDir: path.join(__dirname,("views"), "layouts"),
-    partialsDir: path.join(__dirname,("views"), "partials"),
+    layoutsDir: path.join(__dirname, ("views"), "layouts"),
+    partialsDir: path.join(__dirname, ("views"), "partials"),
     extname: ".hbs",
     helpers: require("./lib/handlebars"),
 })
@@ -26,7 +26,7 @@ const handlebars=exphbs.create({
 //Settings
 app.set('port', process.env.PORT || 4000);
 app.set("views", path.join(__dirname, "views"));
-app.engine( ".hbs",handlebars.engine);
+app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
 // widdlewars
 app.use(morgan("dev"));
@@ -34,36 +34,33 @@ app.use(fileupload())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
-    secret:'julieth',
-    resave:false,
-    saveUninitialized:false,
+    secret: 'julieth',
+    resave: false,
+    saveUninitialized: false,
     store: new mysqlstore(database)
 }))
 app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
-//global
+    //global
 app.use((req, res, next) => {
-    app.locals.message=req.flash('message')
-    app.locals.success=req.flash('success')
-    app.locals.user=req.user
-  next();
+    app.locals.message = req.flash('message')
+    app.locals.success = req.flash('success')
+    app.locals.user = req.user
+    next();
 });
 //public
 app.use(express.static(path.join(__dirname, "public")));
 //routes
 app.use(require('./routes/index.router'))
 app.use(require('./routes/login.router'))
-app.use('/capacitacion',require('./routes/capacitacion.router'))
-app.use('/tipoCapacitacion',require('./routes/tipocapacitacion.router')) 
-app.use('/video',require('./routes/video.router'))
-app.use('/clasificacion',require('./routes/clasificacion.router'))
-app.use('/subclasificacion',require('./routes/subclasificacion.router'))
-<<<<<<< HEAD
-app.use('/objetivo',require('./routes/objetivo.router'))
-=======
+app.use('/capacitacion', require('./routes/capacitacion.router'))
+app.use('/tipoCapacitacion', require('./routes/tipocapacitacion.router'))
+app.use('/video', require('./routes/video.router'))
+app.use('/clasificacion', require('./routes/clasificacion.router'))
+app.use('/subclasificacion', require('./routes/subclasificacion.router'))
+
+app.use('/objetivo', require('./routes/objetivo.router'))
 app.use('/actividad', require('./routes/actividad.router'))
 app.use('/videoActividad', require('./routes/videoActividad.router'))
->>>>>>> 66201cfb3a2d09eef38be5e7ccb979e7a4338c38
-module.exports=app
-
+module.exports = app
